e4501241644f6fba1a1e4dbd78fc483c53cc6167
Kangarroar <pedrosandm@hotmail.com>
2025-10-29 15:43:53 -0300

    Fixes.

--- Lenovo Fan Controller/App.xaml.cs
+++ Lenovo Fan Controller/App.xaml.cs
@@ -6,7 +6,7 @@ namespace Lenovo_Fan_Controller
 {
     public partial class App : Application
     {
-        public static string FanControlPath { get; private set; } //STOP CRASHING?????????
+        public static string FanControlPath { get; private set; } 
         public static string BalancedConfigPath { get; private set; }
         public static string PerformanceConfigPath { get; private set; }
         public static string QuietConfigPath { get; private set; }
@@ -38,13 +38,13 @@ namespace Lenovo_Fan_Controller
 fan_curve_points : 5
 fan_accl_value : 2
 fan_deccl_value : 2
-fan_rpm_points : 0 0 2200 3600 3900
-cpu_temps_ramp_up : 11 45 55 60 65
-cpu_temps_ramp_down : 10 43 53 58 63
-gpu_temps_ramp_up : 11 50 55 60 63
-gpu_temps_ramp_down : 10 48 53 58 61
-hst_temps_ramp_up : 11 50 55 65 70
-hst_temps_ramp_down : 10 48 53 63 68";
+fan_rpm_points : 1000 1500 2200 3600 4400
+cpu_temps_ramp_up : 31 45 55 60 65
+cpu_temps_ramp_down : 30 43 53 58 63
+gpu_temps_ramp_up : 31 50 55 60 63
+gpu_temps_ramp_down : 30 48 53 58 61
+hst_temps_ramp_up : 31 50 55 65 70
+hst_temps_ramp_down : 30 48 53 63 68";
 
             CreateConfigIfNeeded(BalancedConfigPath, defaultConfig);
             CreateConfigIfNeeded(PerformanceConfigPath, defaultConfig);
